4bee548f4e9388c8470936e92abf35c137c0a24c
Fernando Lisboa <55773308+fernandollisboa@users.noreply.github.com>
2022-03-11 17:17:00 -0300

    feedback: code review

--- src/routes/categoriesRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-const categoriesRouter = Router();
-import { getCategories, postCategories } from '../controllers/categoryController.js'
-import { validateCategorySchema } from '../middleware/validateCategorySchema.js'
-
-categoriesRouter.get('/categories', getCategories);
-categoriesRouter.post('/categories', validateCategorySchema, postCategories);
-
-export default categoriesRouter;
